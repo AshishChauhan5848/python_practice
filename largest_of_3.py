eaf7904a1fab25858ba3c87914f29550ada42bd7
Ashish Chauhan <chauhanashish5555@gmail.com>
2025-05-29 05:46:53 +0530

    largets of three number

---
+++ largest_of_3.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a 1st number: "))
+b = int(input("Enter a 2nd number: "))
+c = int(input("Enter a 3rd number: "))
+
+largest = max(a,b,c)
+
+print("largets of three no is : ", largest)
